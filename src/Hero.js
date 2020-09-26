4d2d6720d55622617beb82ea1cf4ef9f1c86e312
Tasnimul H. Tauhid <thtauhid.71@gmail.com>
2020-09-27 02:43:36 +0600

    Hero Section

---
+++ src/Hero.js
@@ -0,0 +1,16 @@
+import React from "react"
+import "./Hero.css"
+
+function Hero() {
+  return (
+    <div className="hero">
+      <div className="texts">
+        <span className="initial">Hi, </span>
+        <span className="myname">Tauhid</span>
+        <span className="closing"> Here</span>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
