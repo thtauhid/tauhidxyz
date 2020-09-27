a9340d70f89a80ff04c79bd1c5ae077fd1054165
Tasnimul H. Tauhid <thtauhid.71@gmail.com>
2020-09-28 03:50:48 +0600

    Skills component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react"
 import "./App.css"
 import Hero from "./Hero"
+import Skills from "./Skills"
 
 function App() {
   return (
     <div className="App">
       <Hero />
+      <Skills />
     </div>
   )
 }
