109cbbce3bee9d540d6dcdf93a7955713b3977f2
Tasnimul H. Tauhid <thtauhid.71@gmail.com>
2020-09-28 03:57:10 +0600

    Cleanup

--- src/ReactIcon.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import { ReactJs } from "@icons-pack/react-simple-icons"
-
-function ReactIcon() {
-  return <ReactJs color="#61DAFB" size={24} />
-}
-
-export default ReactIcon
